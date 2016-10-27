f9e3be6b2a2fd383deeb04919c62eea4b87e8caa
Jordan Yelloz <jordan.yelloz@my.csun.edu>
2016-10-27 14:04:23 -0700

    Added demo data as JSON.

---
+++ demo-data.js
@@ -0,0 +1,23 @@
+window.DEMO_DATA = [
+  {
+    "id": "94f9e214-a0f3-415c-a31f-595d74660424",
+    "responses": [],
+    "title": "Einst\u00fcrzende Neubauten - Nagorny Karabach-36559623"
+  },
+  {
+    "id": "2ae055ea-712c-45b6-a533-74cb8bf9aae1",
+    "responses": [],
+    "title": "Your alternate news site sucks"
+  },
+  {
+    "title": "Why Django Sucks PyCon SE 2015",
+    "responses": [
+      {
+        "position": 22.3,
+        "id": "e81ba8f4-3dbf-4444-bcb8-a3825dc5a829",
+        "title": "You have 20 seconds to comply Robocop"
+      }
+    ],
+    "id": "4eef93d3-b609-4300-864e-4ce60fe1a908"
+  }
+];
